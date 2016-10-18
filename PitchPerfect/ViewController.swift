9a74a4385973a012540adf9ac53e6a709c072e44
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2016-10-17 19:10:35 -0500

    Enabling/Disabling record/stop record buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,26 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordingButton: UIButton!
+    
     @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopRecordingButton.enabled = false
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func viewWillAppear(animated: Bool) {
-        print("Hello World")
-    }
-
 
     @IBAction func recordAudio(sender: AnyObject) {
     
         recordingLabel.text = "Recording in progress"
+        stopRecordingButton.enabled = true
+        recordingButton.enabled = false
     }
 
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("Stop recording button pressed")
+        recordingLabel.text = "Tap to record"
+        stopRecordingButton.enabled = false
+        recordingButton.enabled = true
+        
+    
+    
+    }
 }
 
