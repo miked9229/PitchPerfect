2c7c5445c7a9fabe288c020c176393ec15afbeb9
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2016-10-28 17:27:01 -0500

    Able to Play Sound

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,13 +7,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
+    
     var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int { case Echo = 0, Reverb, Fast, Slow, Chipmunk, Vader }
+    
+    @IBAction func playSoundForButton(sender: AnyObject) {
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        case .Fast:
+            playSound(rate:1.5)
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .Vader:
+            playSound(pitch:-1000)
+        
+        }
+        configureUI(.Playing)
+        stopButton.enabled = true
+        
+    }
+    
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("Stop audio button pressed")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("PlaysoundsViewController loaded")
+        setupAudio()
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +68,9 @@ class PlaySoundsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    override func viewWillAppear(animated: Bool) {
+        configureUI(.NotPlaying)
     }
     
 
